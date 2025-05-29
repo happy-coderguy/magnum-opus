73186fe1858054289360a907f0960d7d749bfc94
Happy Coderguy <21szili@parmiters.herts.sch.uk>
2025-05-29 17:28:40 +0000

    Began creating menu.

---
+++ script.js
@@ -0,0 +1,6 @@
+const start_menu = document.querySelector(".start_menu");
+const smenu_play = document.querySelector(".smenu_play");
+const smenu_settings = document.querySelector(".smenu_settings");
+const smenu_help = document.querySelector(".smenu_help");
+
+const settings = document.querySelector(".settings");
