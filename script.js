aa6dfe410454563b8465276481a9a67fc94b7def
Happy Coderguy <21szili@parmiters.herts.sch.uk>
2025-06-04 07:16:03 +0000

    The game loads now

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**DOM stuff**/
+const root = document.querySelector(":root");
+
 const start_menu = document.querySelector(".start_menu");
 const smenu_play = document.querySelector(".smenu_play");
 const smenu_settings = document.querySelector(".smenu_settings");
@@ -9,10 +12,79 @@ const set_close = document.querySelector(".set_close");
 const help = document.querySelector(".help");
 const help_close= document.querySelector(".help_close");
 
+const game_div = document.querySelector(".game");
+
+const loader = document.querySelector(".loader");
+const loader_cover = document.querySelector(".loader_cover");
+const lc_text = document.querySelector(".lc_text");
+const lc_image = document.querySelector(".lc_image");
+// ...existing code...
+
+const loading_images = [
+    "backgrounds/black.jpg",
+    "backgrounds/menu.png",
+    "humans/archer.png",
+    "humans/gun_car.png",
+    "humans/horseman.png",
+    "humans/juggernaut.png",
+    "humans/labourer.png",
+    "humans/musketman.png",
+    "humans/spearman.png",
+    "humans/swordsman.png",
+    "humans/tank.png",
+    "pastans/farfalle.png",
+    "pastans/fusilli.png",
+    "pastans/lasagna.png",
+    "pastans/macaroni.png",
+    "pastans/orechiette.png",
+    "pastans/penne.png",
+    "pastans/rigatoni.png",
+    "pastans/spaghetti.png",
+    "pastans/tagliatelle.png",
+    "scrapbots/annihilator.png",
+    "scrapbots/builder.png",
+    "scrapbots/destroyer.png",
+    "scrapbots/fighter.png",
+    "scrapbots/fodder.png",
+    "scrapbots/pursuer.png",
+    "scrapbots/shooter.png",
+    "scrapbots/skirmisher.png",
+    "scrapbots/sprinter.png",
+    "tiles/yox_empire_hq.png",
+    "tiles/scrapbots_hq.png",
+    "tiles/pastans_hq.png",
+    "tiles/oil.png",
+    "tiles/oil_developed.png",
+    "tiles/mine.png",
+    "tiles/mine_developed.png",
+    "tiles/land.png",
+    "tiles/humans_hq.png",
+    "tiles/hazardite.png",
+    "tiles/hazardite_developed.png",
+    "tiles/gems.png",
+    "tiles/gems_developed.png",
+    "tiles/food.png",
+    "tiles/food_developed.png",
+    "tiles/aluminium.png",
+    "tiles/aluminium_developed.png",
+    "yox_empire/strider.png",
+    "yox_empire/slingslime.png",
+    "yox_empire/reaper.png",
+    "yox_empire/lich.png",
+    "yox_empire/leviathan.png",
+    "yox_empire/kobold.png",
+    "yox_empire/hoplite.png",
+    "yox_empire/gnome.png",
+    "yox_empire/cerberus.png"
+];
+
+//this is for goofy easter egg
 const d = new Date
 let time = d.getTime();
 
+/**FUNCTIONS**/
 function show_settings(){
+    //code for goofy easter egg
     const e = new Date
     if ((time+178)==e.getTime()){
         settings.style.background="linear-gradient(#ff0000, #00ff00)";
@@ -20,6 +92,7 @@ function show_settings(){
     else{
         settings.style.background="#44ee44";
     }
+    //actual code for showing settings
     settings.style.display="block";
 }
 function hide_settings(){
@@ -31,8 +104,41 @@ function show_help(){
 function hide_help(){
     help.style.display="none";
 }
+function game_start(){
+    start_menu.style.display="none";
+    settings.style.display="none";
+    help.style.display="none";
+    game_div.style.display="block";
+
+    load_assets();
+}
+function load_assets(){
+    loader_cover.style.display="block";
+    loader.style.display="block";
+    linknum = 0;
+    function next_image(){
+        loader.src = "images/"+loading_images[linknum];
+        linknum++;
+        if(linknum < loading_images.length){
+            setTimeout(next_image, 100);    
+        }
+        else{
+            setTimeout(game_init , 100);
+        }
+    }
+    next_image();
+}
+function game_init(){
+    loader_cover.style.display="none";
+    loader.style.display="none";
+    root.style.setProperty("--back-image", "url(images/backgrounds/black.jpg)");
+}
+
+
+
+/**ONCLICK ASSIGNMENTS**/
 smenu_settings.onclick = show_settings;
 set_close.onclick = hide_settings;
 smenu_help.onclick = show_help;
 help_close.onclick = hide_help;
-
+smenu_play.onclick= game_start;
